9e50131179277aa957104d5d75d551ad27be9189
gz052876 <you@example.com>
2020-05-16 10:53:50 +0800

    add quantive trade

--- matplotlib/basic_stock_data_manipulation.py
+++ matplotlib/basic_stock_data_manipulation.py
@@ -9,17 +9,17 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 df = pd.read_csv('uso.csv', parse_dates=True, index_col=0)
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-df['20ma'] = df['Adj Close'].rolling(window=20, min_periods=0).mean()
+df['200ma'] = df['Adj Close'].rolling(window=200, min_periods=0).mean()
+df['50ma'] = df['Adj Close'].rolling(window=50, min_periods=0).mean()
 df.to_csv('uso_ma.csv')
 df=pd.read_csv('uso_ma.csv', parse_dates=True, index_col=0)
-ma100=df['100ma']
-ma20=df['20ma']
+ma200=df['200ma']
+ma50=df['50ma']
 close=df['Adj Close']
 fig = plt.figure()
 ax = fig.add_axes([0.1,0.1,0.8,0.8])
 ax.plot(close, label='close price')
-ax.plot(ma100, '--*', label='100ma')
-ax.plot(ma20, '--*', label='20ma')
+ax.plot(ma200, '--*', label='200ma')
+ax.plot(ma50, '--*', label='50ma')
 ax.legend(loc='best')
 plt.show()
--- matplotlib/getting_stock_price_data.py
+++ matplotlib/getting_stock_price_data.py
@@ -8,6 +8,6 @@ import pandas_datareader.data as web
 
 plt.style.use('ggplot')
 start = dt.datetime(2019, 11, 30)
-end = dt.datetime(2020, 5, 1)
-df = web.DataReader('USO', 'yahoo', start, end)
-df.to_csv('uso.csv')
+end = dt.datetime(2020, 5, 16)
+df = web.DataReader('VXX', 'yahoo', start, end)
+df.to_csv('vxx.csv')
